754a882f04a277aa806c72a948c0c7287705b327
chenjunfei <chenjunfei@youbangsoft.com>
2019-02-26 01:18:38 +0800

    1

--- src/main/java/com/panda521/utils/annotation/UseCaseTracer.java
+++ src/main/java/com/panda521/utils/annotation/UseCaseTracer.java
@@ -7,33 +7,34 @@ import java.lang.reflect.Method;
 
 /**
  * Created by Mr.ChenJF on 2019/2/11.
+ *
  * @UseCase 的处理器
  */
 public class UseCaseTracer {
-    public void handle(Class c1){
+    public void handle(Class c1) {
         //1.get method
         Method[] ms = c1.getMethods();
-        //2.get method's annotation
-        if(ms==null||ms.length<1){
-            System.out.println(c1.getName()+"没有方法");
+        if (ms == null || ms.length < 1) {
+            System.out.println(c1.getName() + "没有方法");
             return;
         }
-        //3.get annotation's value
-        for(Method m:ms){
+        //2.get method's annotation
+        for (Method m : ms) {
             Annotation[] annotations = m.getDeclaredAnnotations();
-            if(annotations==null||annotations.length<1){
+            if (annotations == null || annotations.length < 1) {
                 continue;
             }
-            for (Annotation annotation:annotations){
-                if(annotation instanceof UseCase){
+            //3.get annotation's value
+            for (Annotation annotation : annotations) {
+                if (annotation instanceof UseCase) {
                     UseCase useCase = (UseCase) annotation;
-                    System.out.println(useCase.name()+useCase.description());
+                    System.out.println(useCase.name() + useCase.description());
                 }
             }
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         UseCaseTracer useCaseTracer = new UseCaseTracer();
         useCaseTracer.handle(UserCaseAnnoPrac.class);
     }
